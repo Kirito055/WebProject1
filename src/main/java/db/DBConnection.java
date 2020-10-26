907bab00d01f00a4b69340e730b28298a9b2bd31
майа хуху <dariya.bigabulova@gmail.com>
2020-10-26 17:59:31 +0600

    back for clubs

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,11 +1,13 @@
 package db;
 
+import models.Club;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class DBConnection {
 
@@ -26,6 +28,37 @@ public class DBConnection {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    protected ArrayList<Club> clubSelect(Connection connection)
+    {
+        ArrayList<Club> clubs = new ArrayList();
+        try
+        {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM clubs");
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
+            Club club;
+            while (resultSet.next())
+            {
+                String[] clubFields = new String[numberOfColumns];
+                for(int a=1; a<=numberOfColumns; a++)
+                {
+                    clubFields[a-1] = resultSet.getObject(a).toString();
+                }
+                club = new Club(clubFields);
+                clubs.add(club);
+            }
+            resultSet.close();
+            connection.close();
+            statement.close();
+        }
+        catch (SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+        }
+        return clubs;
     }
 
 }
