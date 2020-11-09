1bbea2aa20ffe5da46c6b4f7c642025c9f25d1e1
82530 <T.baimaganbetov@astanait.edu.kz>
2020-11-10 00:37:16 +0600

    Events

--- src/main/java/models/Club.java
+++ src/main/java/models/Club.java
@@ -1,21 +1,23 @@
 package models;
 
 public class Club {
-    private int id;
-    private String name;
-    private int leader_id;
-    private String logo;
-    private String description;
+        private int id;
+        private String name;
+        private int leader_id;
+        private String logo;
+        private  String description;
 
-    public Club(String[] clubFields) {
-        if (clubFields.length == 5) {
-            this.id = Integer.parseInt(clubFields[0]);
-            this.name = clubFields[1];
-            this.leader_id = Integer.parseInt(clubFields[2]);
-            this.logo = clubFields[3];
-            this.description = clubFields[4];
+        public Club(String[] clubFields)
+        {
+            if(clubFields.length == 5)
+            {
+                this.id = Integer.parseInt(clubFields[0]);
+                this.name = clubFields[1];
+                this.leader_id = Integer.parseInt(clubFields[2]);
+                this.logo = clubFields[3];
+                this.description=clubFields[4];
+            }
         }
-    }
 
     public Club(String name, int leader_id, String logo, String description) {
         this.name = name;
--- src/main/java/servlets/ClubServlet.java
+++ src/main/java/servlets/ClubServlet.java
@@ -1,6 +1,5 @@
 package servlets;
 
-import controllers.ClubController;
 import db.DBConnection;
 import models.Club;
 
@@ -14,8 +13,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ClubServlet extends HttpServlet {
-    private final ClubController clubControl = new ClubController();
-
+    DBConnection db=new DBConnection();
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -23,7 +21,17 @@ public class ClubServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("clubs", clubControl.getAll());
+        try
+        {
+            Connection connection = db.getConnection();
+            ArrayList<Club> clubs = db.clubSelect(connection);
+            connection.close();
+            request.setAttribute("clubs", clubs);
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace();
+        }
         request.getRequestDispatcher("club.jsp").forward(request, response);
     }
 }
