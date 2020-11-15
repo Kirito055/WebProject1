e0399592aa2d9c5299a49d7d202db5d14a4f061b
82530 <T.baimaganbetov@astanait.edu.kz>
2020-11-15 15:12:22 +0600

    PostServlet + updated

--- src/main/java/servlets/EventServlet.java
+++ src/main/java/servlets/EventServlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
+import client.ClubClient;
 import client.PostClient;
+import models.Club;
+import models.Post;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,55 +15,28 @@ public class EventServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String submit = req.getParameter("submit");
-        switch (submit) {
-            case "add": {
-                String title = req.getParameter("title");
-                String description = req.getParameter("userId");
-                String date = req.getParameter("date");
-                String image = req.getParameter("image");
-                int user_id = Integer.parseInt(req.getParameter("user_id"));
-                int club_id = Integer.parseInt(req.getParameter("club_id"));
 
-               // postControl.addEvent(new Post(title, description, date, image, user_id, club_id, "event"));
-
-                break;
-            }
-            case "update": {
-                String id = req.getParameter("id");
-                String title = req.getParameter("title");
-                String description = req.getParameter("userId");
-                String date = req.getParameter("date");
-                String image = req.getParameter("image");
-                int user_id = Integer.parseInt(req.getParameter("user_id"));
-                int club_id = Integer.parseInt(req.getParameter("club_id"));
-
-              //  postControl.updateEvent(new Post(Integer.parseInt(id),title, description, date, image, user_id, club_id, "event"));
-
-                break;
-            }
-            case "delete": {
-                String id = req.getParameter("id");
-
-               // postControl.removeEvent(Integer.parseInt(id));
-
-                break;
-            }
-
-            default: {
-                req.setAttribute("events", "s");
-                break;
-            }
-        }
-        doGet(req, resp);
     }
 
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-      request.setAttribute("eventsALL", PostClient.getAll());
+        String button = request.getParameter("action");
+        System.out.println(button);
 
-        request.getRequestDispatcher("event.jsp").forward(request, response);
+        if(button==null){
+            request.setAttribute("eventsALL", ClubClient.getAll());
+            request.getRequestDispatcher("event.jsp").forward(request, response);
+        }
+        else {
+            if(button.equals("edit")){
+                long id = Long.parseLong(request.getParameter("id"));
+                Post post = PostClient.get(id);
+                System.out.println(post);
+                request.setAttribute("eventsALL", post);
+                request.getRequestDispatcher("update_post.jsp").forward(request, response);
+            }
+        }
     }
 }
--- web/search.jsp
+++ web/search.jsp
@@ -11,12 +11,13 @@
             <select name="group" id="group">
                 <option value="none">None</option>
                 <option value="IT_1911">IT_1911</option>
-                <option value="SE-1907">IT_1911</option>
-                <option value="SE-1908">IT_1911</option>
+                <option value="SE-1907">SE-1907</option>
+                <option value="SE-1908">SE-1908</option>
                 <!--
             тут можно добавить цикл форич
             для вывода всех груп
         -->
+
             </select>
         </div>
 
@@ -44,7 +45,33 @@
             <h4>Name</h4>
             <p>Group/Year</p>
         </div>
+        <c:forEach var="user" items="${users}" >
+            <form action="${pageContext.request.contextPath}/search" method="post">
+                <div id="club-<c:out value="${user.id}"/>">
+                    <!-- Single course -->
+                    <div >
+                        <div class="course-img">
+                            <img src="<c:out value="${user.avatar}"/>" alt="Foto" width="350px" />
+                        </div>
+                        <div >
+                            <div >
+                                <h4><input class="border p-2 w-100 my-2" type="text" name="name" placeholder="first name" value="<c:out value="${user.firstName}+${user.lastName}"/>"></h4>
+                                <input class="border p-2 w-100 my-2" type="text" name="birthDate" placeholder="birthDate" value="<c:out value="${user.birthDate}"/>"><br>
+                                <input class="border p-2 w-100 my-2" type="text" name="grade" placeholder="grade" value="<c:out value="${user.grade}"/>"><br>
+                                <input class="border p-2 w-100 my-2" type="text" name="group" placeholder="group" value="<c:out value="${user.group}"/>"><br>
 
+                            </div>
+                            <div class="course-cap-bottom d-flex justify-content-between">
+                                <ul>
+                                    <a href="${pageContext.request.contextPath}/user"><li>Edit</li></a>
+                                </ul>
+                                <a href="${pageContext.request.contextPath}/user"><span>Delete</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </c:forEach>
     </div>
 
 </div>
