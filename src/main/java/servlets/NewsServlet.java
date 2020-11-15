b116da1a607ddd0f3d2e8a8ca8ff1caad164f2fc
82530 <T.baimaganbetov@astanait.edu.kz>
2020-11-15 15:19:19 +0600

    PostServlet + updated

--- src/main/java/servlets/NewsServlet.java
+++ src/main/java/servlets/NewsServlet.java
@@ -2,7 +2,6 @@ package servlets;
 
 import client.ClubClient;
 import client.PostClient;
-import models.Club;
 import models.Post;
 
 import javax.servlet.ServletException;
@@ -11,32 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class EventServlet extends HttpServlet {
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
+public class NewsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         String button = request.getParameter("action");
         System.out.println(button);
 
         if(button==null){
-            request.setAttribute("eventsALL", PostClient.getAll());
-            request.getRequestDispatcher("event.jsp").forward(request, response);
+            request.setAttribute("newsAll", PostClient.getAll());
+            request.getRequestDispatcher("news.jsp").forward(request, response);
         }
         else {
             if(button.equals("edit")){
                 long id = Long.parseLong(request.getParameter("id"));
                 Post post = PostClient.get(id);
                 System.out.println(post);
-                request.setAttribute("eventsALL", post);
+                request.setAttribute("newsAll", post);
                 request.getRequestDispatcher("update_post.jsp").forward(request, response);
             }
         }
+
     }
+
 }
